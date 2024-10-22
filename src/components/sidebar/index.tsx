0b9742552b3e425fe0d99e1f1ff19780386aa6ea
BaitemirAsanbaev <baitemir1706@gmail.com>
2024-10-23 01:05:04 +0600

    project structure prepared

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface ISidearProps {
+}
+
+const Sidear: React.FunctionComponent<ISidearProps> = ({}) => {
+  return <div></div>;
+};
+
+export default Sidear;
